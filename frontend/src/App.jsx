22fa5da3e7d89a4aabaa24272d5e5fdfb0d9365a
abhay-raj19 <rajpootabhay423@gmail.com>
2024-01-31 17:16:11 +0530

    Feat:routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Dashoard from "./components/Dashoard";
+import SendMoney from "./components/SendMoney";
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashoard/>} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
