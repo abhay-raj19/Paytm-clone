8df24f64f9e691889f2b8de6d959b781d0606645
abhay-raj19 <rajpootabhay423@gmail.com>
2024-03-21 06:53:23 +0530

    minor fixes
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashoard from "./pages/Dashoard";
 import SendMoney from "./pages/SendMoney";
+import Landing from "./pages/Landing";
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashoard/>} />
